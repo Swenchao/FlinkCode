f103e72534a6a07b1587045c11a2ebfde16bd005
Swenchao <scwrite678@outlook.com>
2021-03-01 20:53:55 +0800

    富函数使用——Swenchao

--- src/main/java/com/swenchao/apitest/transform/TransformTest5_RichFunction.java
+++ src/main/java/com/swenchao/apitest/transform/TransformTest5_RichFunction.java
@@ -2,9 +2,13 @@ package com.swenchao.apitest.transform;
 
 import com.swenchao.apitest.beans.SensorReading;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import scala.Tuple2;
 
 /**
  * @author: Swenchao
@@ -15,7 +19,7 @@ public class TransformTest5_RichFunction {
     public static void main(String[] args) throws Exception {
         // 创建环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(1);
+        env.setParallelism(4);
 
         // 读取数据进行转换
         DataStreamSource<String> sensorDataStreamSource = env.readTextFile("src/main/resources/sensor.txt");
@@ -26,7 +30,28 @@ public class TransformTest5_RichFunction {
                 return new SensorReading(details[0], Long.valueOf(details[1]), Double.valueOf(details[2]));
             }
         });
-
+        DataStream<Tuple2<String, Integer>> resultStream = sensorDataStream.map(new MyMapFunction());
+        resultStream.print();
         env.execute();
+    }
+
+    public static class MyMapFunction extends RichMapFunction<SensorReading, Tuple2<String, Integer>> {
+
+        @Override
+        public Tuple2<String, Integer> map(SensorReading sensorReading) throws Exception {
+            return new Tuple2<>(sensorReading.getId(), getRuntimeContext().getIndexOfThisSubtask());
+        }
+
+        // 进行初始化操作
+        @Override
+        public void open(Configuration configuration) throws Exception {
+            System.out.println("open");
+        }
+
+        // 进行一些结束操作
+        @Override
+        public void close() throws Exception {
+            System.out.println("close");
+        }
     }
 }
