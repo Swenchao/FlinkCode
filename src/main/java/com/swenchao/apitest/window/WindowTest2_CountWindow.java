2f1ed3cc5c1a104a3a4a6873743cd1ac3e38aa99
Swenchao <scwrite678@outlook.com>
2021-05-05 19:23:37 +0800

    Flink-CountWindow——Swenchao

--- src/main/java/com/swenchao/apitest/window/WindowTest2_CountWindow.java
+++ src/main/java/com/swenchao/apitest/window/WindowTest2_CountWindow.java
@@ -1,10 +1,14 @@
 package com.swenchao.apitest.window;
 
 import com.swenchao.apitest.beans.SensorReading;
+import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Author: swenchao
@@ -16,6 +20,7 @@ public class WindowTest2_CountWindow {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
+
 
         // socket文本流
         DataStreamSource<String> sensorDataStreamSource = env.socketTextStream("localhost", 7777);
@@ -29,6 +34,35 @@ public class WindowTest2_CountWindow {
             }
         });
 
+        // 开计数窗口测试（）
+//        sensorDataStreamSource.keyBy("id")
+                // 10个数一个窗口，隔两个数开一个窗口
+//                .countWindow(10, 2)
+//                .aggregate()
+
         env.execute();
+    }
+
+    public static class MyAvgTemp implements AggregateFunction<SensorReading, Tuple2<Double, Integer>, Double> {
+
+        @Override
+        public Tuple2<Double, Integer> createAccumulator() {
+            return new Tuple2<>(0.0, 0);
+        }
+
+        @Override
+        public Tuple2<Double, Integer> add(SensorReading sensorReading, Tuple2<Double, Integer> accu) {
+            return new Tuple2<>(accu.f0 + sensorReading.getTemperature(), accu.f1 + 1);
+        }
+
+        @Override
+        public Double getResult(Tuple2<Double, Integer> accu) {
+            return accu.f0 / accu.f1 * 1.0;
+        }
+
+        @Override
+        public Tuple2<Double, Integer> merge(Tuple2<Double, Integer> a, Tuple2<Double, Integer> b) {
+            return new Tuple2<>(a.f0 + b.f0, a.f1 + b.f1);
+        }
     }
 }
